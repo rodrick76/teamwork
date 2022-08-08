e054bbf8a96a31da00cbd7a66209e37862d61d12
BERYL ANNA <108726487+BERYL18@users.noreply.github.com>
2022-08-08 14:29:55 +0100

    changes

--- my-project/src/component/Home.jsx
+++ my-project/src/component/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Products from './Products';
 
 
 
 const Home = () => {
   return (
      <div >
-         <div className='w-full flex mt-24 text-24'>
+         <div className='flex w-full mt-24 text-24'>
           <h1>NEW ARRIVALS</h1>
           </div>
           <p >your satisfaction, Our priority</p>
          
-
+   <Products/>
      </div>
   )
 }
