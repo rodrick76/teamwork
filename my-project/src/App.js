537e81df15adbcc7183b96b67595e6bf868da87e
rodrick76 <109237129+rodrick76@users.noreply.github.com>
2022-08-08 05:43:02 -0700

    adjustment
    aranging

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './component/Navbar';
 import './App.css';
+import Home from './component/Home';
 
 function App() {
   return (
    <>
     <Navbar/>
+    <Home/>
    </>
   );
 }
