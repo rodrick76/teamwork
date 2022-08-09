4831b6aef3267c85d6fed1d02cb625c1cafd0c76
rodrick76 <109237129+rodrick76@users.noreply.github.com>
2022-08-09 02:22:54 -0700

    new

--- my-project/src/component/Products.jsx
+++ my-project/src/component/Products.jsx
@@ -56,25 +56,24 @@ const ShowProducts = ({ filter = [] }) => {
                 <button className="h-10 px-4 py-2 m-4 bg-orange-300 rounded-md btn btn-outline-dark hover:bg-gray-800">Women's Clothing</button>
                 <button className="h-10 px-4 py-2 m-4 bg-orange-300 rounded-md btn btn-outline-dark hover:bg-gray-800">Jewelery</button>
                 <button className="h-10 px-4 py-2 m-4 bg-orange-300 rounded-md btn btn-outline-dark hover:bg-gray-800">Electronics</button>
-                        <div className="container text-center">
-                            <div className='row'>
-                {filter.map((product) => {
-                    return (
-                                <div class='col-md-3'>
-                                    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
+                <div className="container text-center">
+                    <div className='row'>
+                        {filter.map((product) => {
+                            return (
+                                <div className='col-md-3'>
+                                    <div class="card">
+                                        <img src={product.image} class="card-img-top" alt={product.title} />
+                                        <div class="card-body">
+                                            <h5 class="card-title">{product.title}</h5>
+                                            <p class="card-text">{product.price}</p>
+                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                    </div>
                                 </div>
-                    );
-                })}
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
             </div>
         </>
     );
